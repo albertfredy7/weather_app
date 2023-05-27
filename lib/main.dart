e0e5d9fcc98fbd15d3ab7e93a3e29dbccc040cbf
albertfredy7 <albertfredy007@gmail.com>
2023-05-27 16:47:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
+        fontFamily: 'Montserrat',
+        textTheme: TextTheme(
+          headline6: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+          ),
+          bodyText2: TextStyle(fontSize: 18, color: Colors.white70),
+        ),
+      ),
+      home: WeatherPage(),
+    );
+  }
+}
+
+class WeatherPage extends StatefulWidget {
+  @override
+  _WeatherPageState createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  Position? _currentPosition;
+  String _weather = '';
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      setState(() {
+        _currentPosition = position;
+      });
+
+      getWeather();
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future<void> getWeather() async {
+    try {
+      if (_currentPosition != null) {
+        final apiKey = '8a7081ed009245ba88e72553232705';
+        final url =
+            'https://api.weatherapi.com/v1/current.json?key=$apiKey&q=${_currentPosition!.latitude},${_currentPosition!.longitude}';
+
+        final response = await http.get(Uri.parse(url));
+
+        if (response.statusCode == 200) {
+          final data = json.decode(response.body);
+          final tempC = data['current']['temp_c'];
+          final condition = data['current']['condition']['text'];
+
+          setState(() {
+            _weather = 'Temperature: $tempC°C\nCondition: $condition';
+            _isLoading = false;
+          });
+        } else {
+          setState(() {
+            _weather = 'Failed to fetch weather data';
+            _isLoading = false;
+          });
+        }
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Weather App',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue.shade200,
+              Colors.blue.shade800,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.wb_sunny,
+                size: 80,
+                color: Colors.white,
+              ),
+              SizedBox(height: 16),
+              if (_isLoading)
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                )
+              else
+                Column(
+                  children: [
+                    Text(
+                      'Current Weather',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      _weather,
+                      style: Theme.of(context).textTheme.bodyText2,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
